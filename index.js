6c7fe63661b97ae83fb4ec8058a98a2cb28881dc
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2025-10-26 13:38:03 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//level 1 of store variable
+var name = "Abdullah"
+console.log(name)
+
+var city = "Karachi"
+console.log(city)
+
+var name = "Abdullah"
+var user = "Hello"
+console.log(name + user)
+
+var country = "Pakistan"
+console.log(country.length)
+
+var my_name = "Abdullah"
+var your_name = "Umer"
+console.log(my_name + your_name)
+
+//level 2 
+var school = "Dawn Public School"
+console.log(school.length)
+
+var empty = ""
+console.log(empty.length)
+
+var greetings = "Hello"
+var number = 10
+console.log(greetings+number)
+
+//level 3
+
+var first_name = "Abdullah"
+var last_name = "Umer"
+var space = " "
+console.log(first_name + space + last_name)
