c5e52fbcee71e22c641028bb26aeffdd8e3de22b
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2025-10-30 07:35:58 -0700

    index.js

---
+++ class 3/index.js
@@ -0,0 +1,19 @@
+var a = 5
+var b = a++
+console.log(a)
+
+var a = 7
+console.log(++a)
+
+var c = 10
+var b = c--
+console.log(c)
+
+var score = 20
+score += 5
+console.log(score)
+
+
+var score = 10
+score -= 3
+console.log(score)
