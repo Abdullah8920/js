ccd17b4560b91374a85e5bd2c11a93712ccebf23
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2025-12-30 22:25:24 +0500

    Your descriptive commit message

--- DOM/index.js
+++ DOM/index.js
@@ -1,12 +1,23 @@
-var btn = document.getElementById("increase");
+var btninc = document.getElementById("increase");
+var btndec = document.getElementById("decrease");
+var btnReset = document.getElementById("reset");
+
 // console.log(btn)
 
 var heading = document.querySelector("h1");
 // console.log(heading)
+var defaultSize = 30;
 
-btn.addEventListener("click", function inc() {
+btninc.addEventListener("click", function inc() {
     // alert("hello respone plz>>>>>>>>>>>>>>", inc)
     var currentsize = parseInt(heading.style.fontSize)
     heading.style.fontSize = (currentsize + 4) + "px";
 })
+btndec.addEventListener("click", function dec() {
+    var currentsize = parseInt(heading.style.fontSize)
+    heading.style.fontSize = (currentsize - 2) + "px";
+})
 
+btnReset.addEventListener("click", function reset() {
+    heading.style.fontSize = defaultSize + "px";
+});
